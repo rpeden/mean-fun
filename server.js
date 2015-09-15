3c852ebf9f9d2d36906736a13520dc966767f214
rpeden <ryan04@gmail.com>
2015-09-15 10:17:46 -0400

    added path to render partials

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(`${__dirname}/public`));
 
+app.get('/partials/:partialPath', (req,res) => {
+  res.render(`partials/${req.params.partialPath}`);
+});
+
 app.get('*', (req, res) => {
   res.render('index');
 });
