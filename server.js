0840af0319ce5f9bc0796f3b7a37541976a12a9d
rpeden <ryan04@gmail.com>
2015-09-15 00:08:31 -0400

    added to server

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const stylus = require('stylus');
-
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const app = express();
 
+const compile = (str, path) => {
+    return stylus(str).set('filename', path);
+}
+
 app.set('views', `${__dirname}/server/views`);
 app.set('view engine', 'jade');
+app.use(stylus.middleware(
+  {
+    src: `${__dirname}/public`,
+    compile: compile
+  }
+));
+app.use(express.static(`${__dirname}/public`));
 
 app.get('*', (req, res) => {
   res.render('index');
